5b7d655ce903d74cf2d62ba663ea571f5ecda286
Marta Jopp <martapeterson@gmail.com>
2018-03-01 10:33:22 -0600

    header component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'; //allows us to write JSX, transpile and use react
+import ReactDOM from 'react-dom'; //allows us to put things on the DOM
+import './index.css'; //brings style into application
+import App from './App'; //brings the component in
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root')); //grab this and replace with App
 registerServiceWorker();
